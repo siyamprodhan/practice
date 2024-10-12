e7aee755156598350a5a8ef534cc228ff56c8b72
siyamprodhan <siyamprodhan630@gmail.com>
2024-10-13 05:04:31 +0600

    experiment

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ let user = {
 console.log(user.id);  // Output: 1234567890123456789012345678901234567890n
 console.log(user.name);  // Output: John Doe
 console.log(user[privateMetadata]);  // Output: { lastLogin: "2024-10-12" }
+
+const orderForcostomer=()=>{
+  console.log('take order for costomer ')
+
+  setTimeout(()=>{
+    console.log('order in cooking');
+    
+  },3000);
+  console.log('order is processed')
+}
+console.log(orderForcostomer());
+console.log('order compleate')
